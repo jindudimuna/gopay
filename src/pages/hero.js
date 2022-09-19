eb61d69813e91da68ab69d18648f3094194d7152
jindudimuna <danieldimuna@gmail.com>
2022-09-19 00:19:57 -0700

    hero page desktop view

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './hero.css'
+import Hero1 from '../assets/user4.JPG'
+import Hero2 from '../assets/user2.JPG'
+import Hero3 from '../assets/user3.JPG'
 
 function Hero(){
 
 return(
-
-    <div>
+<>
+    <div className="hero">
 <div className="hero-text">
 <h1 className="intro-text"> Easy Way To Process Your <span className="highlight">Payments</span></h1> 
 <h1 className="sub-text"> Pay for transactions with ease to anywhere around the world with your mobile device</h1>
@@ -17,17 +20,25 @@ return(
 
 
 
-
 </div>
 
 </div>
 
-<div>
+<div className="hero-images">
+<div className="hero-image-1">
+    <img src={Hero1} alt="White man smiling" className="users"/>
+</div>
+<div className="hero-image-2">
+    <img src={Hero2} alt="Black girl smiling for representation" className="usersF"/>
+    <img src={Hero3} alt="white girl smiling for representation" className="usersF"/>
 
+
+</div>
 </div>
     </div>
 
 
+    </>
 
 
 )
